133a5df0d45f767f8230934f6c8cabeb20530479
fondaTHEhonda <casatchell15@gmail.com>
2021-02-14 17:03:36 -0500

    created function to put array on HTML

--- library.js
+++ library.js
@@ -7,7 +7,9 @@ function Book(title) {
 }
 
 function addToMainLibrary() {
-    //will add contents of form to mainLibrary array
+    //if each form field is filled out
+    //create new Object where each field is .title, .author, etc.
+    //Push new Object into Array
 }
 
 const book1 = new Book("Heat");
@@ -16,3 +18,13 @@ const book2 = new Book("The Partner");
 console.log(book1.title);
 console.log(book2);
 console.log(mainLibrary);
+
+function displayBooks() {
+    for(let i = 0; i < mainLibrary.length; i++) {
+        let newDiv = document.createElement('P');
+        newDiv.textContent = mainLibrary[i];
+        document.getElementById("library").appendChild(newDiv);
+    }
+}
+
+displayBooks();
