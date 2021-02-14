f78ce8bbd30ef05aab6b8f23c86508349845bd65
fondaTHEhonda <casatchell15@gmail.com>
2021-02-14 17:22:00 -0500

    Main library array pushes to HTML page with information

--- library.js
+++ library.js
@@ -1,9 +1,12 @@
 //Basic Set Up -> main array and Book constructor and function that adds it to array
 
-let mainLibrary = ["Heat", "The Partner"];
 
-function Book(title) {
+
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addToMainLibrary() {
@@ -12,8 +15,10 @@ function addToMainLibrary() {
     //Push new Object into Array
 }
 
-const book1 = new Book("Heat");
-const book2 = new Book("The Partner");
+const book1 = new Book("Heat", "Mike Lupica", 100, true);
+const book2 = new Book("The Partner", "John Grisham", 250, true);
+
+let mainLibrary = [book1, book2];
 
 console.log(book1.title);
 console.log(book2);
@@ -21,9 +26,20 @@ console.log(mainLibrary);
 
 function displayBooks() {
     for(let i = 0; i < mainLibrary.length; i++) {
-        let newDiv = document.createElement('P');
-        newDiv.textContent = mainLibrary[i];
+        let newDiv = document.createElement('DIV');
+        let newP = document.createElement('P');
         document.getElementById("library").appendChild(newDiv);
+        newDiv.appendChild(newP);
+        newP.textContent = "Title: " + mainLibrary[i].title + " " + "Author: " + mainLibrary[i].author + " " + "Pages: " + mainLibrary[i].pages;
+        if(mainLibrary[i].read === true) {
+            let newInput = document.createElement("P");
+            newDiv.appendChild(newInput);
+            newInput.textContent = "I have read this book."
+        } else {
+            let newerP = document.createElement("P");
+            newDiv.appendChild(newerP);
+            newerP.textContent = "I have not read this book."
+        }
     }
 }
 
