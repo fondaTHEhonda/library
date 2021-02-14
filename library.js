1862d09ef7a422c87ecde3a6579ee186548e347a
fondaTHEhonda <casatchell15@gmail.com>
2021-02-14 16:45:57 -0500

    recovering from git corruption

--- library.js
+++ library.js
@@ -1,1 +1,18 @@
-//Blank JS File to start.
+//Basic Set Up -> main array and Book constructor and function that adds it to array
+
+let mainLibrary = ["Heat", "The Partner"];
+
+function Book(title) {
+    this.title = title;
+}
+
+function addToMainLibrary() {
+    //will add contents of form to mainLibrary array
+}
+
+const book1 = new Book("Heat");
+const book2 = new Book("The Partner");
+
+console.log(book1.title);
+console.log(book2);
+console.log(mainLibrary);
